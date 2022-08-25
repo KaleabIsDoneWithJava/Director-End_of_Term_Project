d9770355e8c35ee8167a09b4f07251e0fe86df26
kaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-08-25 16:08:22 +0300

    Added the Generic Repository

---
+++ Models/Base/IEntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Director.Models.Base
+{
+   public interface IEntityBase
+    {
+         long Id { get; set; }
+    }
+}
