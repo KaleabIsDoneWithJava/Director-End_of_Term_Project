43799800e5ded7805e0f2e6851cb4b05d31e51d5
kaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-09-16 14:38:04 +0300

    Migrated changes

--- Migrations/20220916113547_Removed the staff table.Designer.cs
+++ Migrations/20220916113547_Removed the staff table.Designer.cs
@@ -4,14 +4,16 @@ using Director.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Director.Migrations
 {
     [DbContext(typeof(SMSContext))]
-    partial class SMSContextModelSnapshot : ModelSnapshot
+    [Migration("20220916113547_Removed the staff table")]
+    partial class Removedthestafftable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
