049e5a37da93b4edc905e9c70917240c7ecd0d75
eyerusalem-bg <eyerusalem.g22@gmail.com>
2022-09-23 10:01:56 +0300

    Adjusted the AddStaff form to reset properly by adding a default value of a null string (unteseted)

---
+++ Controllers/AnnouncementController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Director.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        // GET: AnnouncementController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AnnouncementController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AnnouncementController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnnouncementController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AnnouncementController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AnnouncementController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
