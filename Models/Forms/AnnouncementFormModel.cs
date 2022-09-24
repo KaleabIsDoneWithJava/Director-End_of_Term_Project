f8ec5738fdde00d228549081a47db77e1df647af
eyerusalem-bg <eyerusalem.g22@gmail.com>
2022-09-24 09:14:55 +0300

    Wrote the AnnouncementFormModel class

---
+++ Models/Forms/AnnouncementFormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;;
+
+namespace Director.Models.Forms
+{
+    public class AnnouncementFormModel
+    {
+        [DisplayName("First Name")]
+        public string Title { get; set; }
+
+        [DisplayName("First Name")]
+        public string Details { get; set; }
+
+    }
+}
