82e11db567fedfafdda00cc24ad08663b048ca97
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-09-25 16:31:39 +0300

    Implemented Passannouncement(model) in AddAnnouncement

--- Models/Functions/AddAnnouncement.cs
+++ Models/Functions/AddAnnouncement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Director.Models.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,18 @@ namespace Director.Models.Functions
 {
     public class AddAnnouncement
     {
-        //public 
+        //Identity isn't yet implemented... so there isn't any need to join the Office staff table & Announcement table
+        //After we do the login... we'll have to join those tables and atleast return the name of the office staff personnel
+        //responsible for making the announcement.
+
+        public Announcement PassAnnouncement(AnnouncementFormModel model)
+        {
+            Announcement allAnnouncements = new Announcement();
+            allAnnouncements.Title = model.Title;
+            allAnnouncements.Detail = model.Details;
+
+            return allAnnouncements;
+        } 
 
     }
 }
