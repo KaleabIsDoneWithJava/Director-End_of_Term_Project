39d802a619d085c24bc96839c515b6f354997065
eyerusalem-bg <eyerusalem.g22@gmail.com>
2022-09-29 18:52:25 +0300

    Added Profile page and implemented the right panel with MakeAppointment modal popup done

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Director.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: ProfileController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+        
+        //Probably going to end up only using the above two.
+        
+        // GET: ProfileController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // POST: ProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProfileController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProfileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
